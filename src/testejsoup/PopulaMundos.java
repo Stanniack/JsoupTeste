9516cfaedeb6dea8b9c35a3e6e76385e366a5405
Mateus Vitor <stanniack@gmail.com>
2019-03-02 09:26:17 -0300

    Brincando com mineração em Html com Jsoup

---
+++ src/testejsoup/PopulaMundos.java
@@ -0,0 +1,82 @@
+
+package testejsoup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PopulaMundos {
+    
+    public static List<String> populaMundos(){
+        List<String> worlds = Arrays.asList(
+                "Antica",
+                "Assombra",
+                "Astera",
+                "Belobra",
+                "Bona",
+                "Calmera",
+                "Carnera",
+                "Celebra",
+                "Celesta",
+                "Cosera",
+                "Damora",
+                "Descubra",
+                "Duna",
+                "Epoca",
+                "Estela",
+                "Faluna",
+                "Ferobra",
+                "Firmera",
+                "Funera",
+                "Furia",
+                "Garnera",
+                "Gentebra",
+                "Gladera",
+                "Harmonia",
+                "Helera",
+                "Honbra",
+                "Impera",
+                "Inabra",
+                "Jonera",
+                "Kalibra",
+                "Kenora",
+                "Lobera",
+                "Luminera",
+                "Lutabra",
+                "Macabra",
+                "Menera",
+                "Monza",
+                "Nefera",
+                "Noctera",
+                "Nossobra",
+                "Olera",
+                "Ombra",
+                "Pacera",
+                "Peloria",
+                "Premia",
+                "Pyra",
+                "Quelibra",
+                "Quintera",
+                "Refugia",
+                "Relania",
+                "Relembra",
+                "Secura",
+                "Serdebra",
+                "Solidera",
+                "Talera",
+                "Tortura",
+                "Venebra",
+                "Vita",
+                "Vunira",
+                "Wintera",
+                "Zuna",
+                "Zunera"
+                
+        );
+        
+        return worlds;
+    }
+    
+    
+    
+}
---
+++ src/testejsoup/TesteJSoup.java
@@ -0,0 +1,96 @@
+package testejsoup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class TesteJSoup {
+
+    // Conteúdo na lista começa no índice 5
+    private static final int CONTENT_START = 5;
+    // O último elemento da lista é lixo
+    private static final int TRASH_ELIMINATOR = 1;
+    // O incrementador é 5 devido aos atributos de cada char
+    private static final int INCREMENTOR = 5;
+
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 12;
+    private static final int TOTAL_WORLDS = 62;
+    private static final int NAME = 1;
+    private static final int VOCATION = 2;
+    private static final int LEVEL = 3;
+    private static final int POINTS = 4;
+
+    public static void main(String[] args) {
+        Long tempoInicial = System.currentTimeMillis();
+        List<String> listaDeElementos = new ArrayList<>();
+        List<Character> listaDeChars = new ArrayList<>();
+        List<String> mundos = PopulaMundos.populaMundos();
+
+        for (int i = 0; i < TOTAL_WORLDS; i++) {
+            
+            Long tempoInicialPorMundo = System.currentTimeMillis();
+
+            for (int j = FIRST_PAGE; j <= LAST_PAGE; j++) {
+                String url = "https://www.tibia.com/community/?subtopic=highscores&world=" + mundos.get(i) + "&list=experience&profession=0&currentpage=" + j;
+
+                try {
+
+                    Document arquivoHtml = Jsoup.connect(url).get();
+                    Elements elementosTagEscolhida = arquivoHtml.getElementsByClass("TableContent");
+
+                    for (Element elemento : elementosTagEscolhida) {
+                        listaDeElementos = elemento.getElementsByTag("td").eachText();
+                        //System.out.println(listaDeElementos);
+
+                    }
+
+                } catch (IOException e) {
+                    System.out.println("Erro na execução.");
+                }
+
+                for (int k = CONTENT_START; k < listaDeElementos.size() - INCREMENTOR; k += 5) {
+                    // Tiras as vírgulas da exp
+                    String valorConvertido = listaDeElementos.get(k + POINTS).replace(",", "");
+                    Long valorCvtLong = Long.valueOf(valorConvertido);
+                    
+                    listaDeChars.add(new Character(
+                            listaDeElementos.get(k),
+                            listaDeElementos.get(k + NAME),
+                            listaDeElementos.get(k + VOCATION),
+                            listaDeElementos.get(k + LEVEL),
+                            valorCvtLong,
+                            mundos.get(i))
+                            );
+                }
+
+            }
+        
+            Long tempoFinalPorMundo = System.currentTimeMillis();
+            System.out.println("O servidor de " + mundos.get(i) + " gastou " + ((tempoFinalPorMundo - tempoInicialPorMundo)/1000)
+            + " segundos para ser minerado");
+        }
+        
+        Long tempoFinal = System.currentTimeMillis();
+        System.out.println("O tempo total para minerar todos os servidores foi de " + ((tempoFinal - tempoInicial)/1000)
+        + " segundos");
+        
+        
+        Collections.sort(listaDeChars);
+        
+        for (int i = 0; i < listaDeChars.size() ; i++) {
+            System.out.println((i+1) + " " + listaDeChars.get(i).getName() + " " + listaDeChars.get(i).getLevel() + " " +
+                    listaDeChars.get(i).getWorld());
+            
+        }
+        
+        
+
+    }
+
+}
