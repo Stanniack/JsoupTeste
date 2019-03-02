dba71136ce6d2420d6be2c2f3de119986105cdb7
Mateus Vitor <stanniack@gmail.com>
2019-03-02 12:00:17 -0300

    Código alterado, apenas minerando as profissões; 74.5 mil objetos gerados.

--- src/testejsoup/TesteJSoup.java
+++ src/testejsoup/TesteJSoup.java
@@ -18,6 +18,7 @@ public class TesteJSoup {
     // O incrementador é 5 devido aos atributos de cada char
     private static final int INCREMENTOR = 5;
 
+    private static final int PROFESSION = 4;
     private static final int FIRST_PAGE = 1;
     private static final int LAST_PAGE = 12;
     private static final int TOTAL_WORLDS = 62;
@@ -33,63 +34,69 @@ public class TesteJSoup {
         List<String> mundos = PopulaMundos.populaMundos();
 
         for (int i = 0; i < TOTAL_WORLDS; i++) {
-            
+
             Long tempoInicialPorMundo = System.currentTimeMillis();
 
-            for (int j = FIRST_PAGE; j <= LAST_PAGE; j++) {
-                String url = "https://www.tibia.com/community/?subtopic=highscores&world=" + mundos.get(i) + "&list=experience&profession=0&currentpage=" + j;
+            for (int n = 1; n <= PROFESSION; n++) {
+                Long tempoInicialPorProfissao = System.currentTimeMillis();
+                
+                for (int j = FIRST_PAGE; j <= LAST_PAGE; j++) {
+                    String url = "https://www.tibia.com/community/?subtopic=highscores&world=" + mundos.get(i) + "&list=experience&profession=" + n + "&currentpage=" + j;
 
-                try {
+                    try {
 
-                    Document arquivoHtml = Jsoup.connect(url).get();
-                    Elements elementosTagEscolhida = arquivoHtml.getElementsByClass("TableContent");
+                        Document arquivoHtml = Jsoup.connect(url).get();
+                        Elements elementosTagEscolhida = arquivoHtml.getElementsByClass("TableContent");
 
-                    for (Element elemento : elementosTagEscolhida) {
-                        listaDeElementos = elemento.getElementsByTag("td").eachText();
-                        //System.out.println(listaDeElementos);
+                        for (Element elemento : elementosTagEscolhida) {
+                            listaDeElementos = elemento.getElementsByTag("td").eachText();
+                            //System.out.println(listaDeElementos);
 
+                        }
+
+                    } catch (IOException e) {
+                        System.out.println("Erro na execução.");
                     }
 
-                } catch (IOException e) {
-                    System.out.println("Erro na execução.");
-                }
+                    for (int k = CONTENT_START; k < listaDeElementos.size() - INCREMENTOR; k += 5) {
+                        // Tiras as vírgulas da exp
+                        String valorConvertido = listaDeElementos.get(k + POINTS).replace(",", "");
+                        Long valorCvtLong = Long.valueOf(valorConvertido);
 
-                for (int k = CONTENT_START; k < listaDeElementos.size() - INCREMENTOR; k += 5) {
-                    // Tiras as vírgulas da exp
-                    String valorConvertido = listaDeElementos.get(k + POINTS).replace(",", "");
-                    Long valorCvtLong = Long.valueOf(valorConvertido);
-                    
-                    listaDeChars.add(new Character(
-                            listaDeElementos.get(k),
-                            listaDeElementos.get(k + NAME),
-                            listaDeElementos.get(k + VOCATION),
-                            listaDeElementos.get(k + LEVEL),
-                            valorCvtLong,
-                            mundos.get(i))
-                            );
-                }
+                        listaDeChars.add(new Character(
+                                listaDeElementos.get(k),
+                                listaDeElementos.get(k + NAME),
+                                listaDeElementos.get(k + VOCATION),
+                                listaDeElementos.get(k + LEVEL),
+                                valorCvtLong,
+                                mundos.get(i))
+                        );
+                    }
 
-            }
-        
+                } // for das páginas
+                
+                Long tempoFinalPorProfissao = System.currentTimeMillis();
+                System.out.println("A profissão " + n + " foi minerada com " + 
+                        ((tempoFinalPorProfissao - tempoInicialPorProfissao)/1000) + " segundos.");
+            } // for das profissões
+
             Long tempoFinalPorMundo = System.currentTimeMillis();
-            System.out.println("O servidor de " + mundos.get(i) + " gastou " + ((tempoFinalPorMundo - tempoInicialPorMundo)/1000)
-            + " segundos para ser minerado");
-        }
-        
+            System.out.println("O servidor de " + mundos.get(i) + " gastou " + ((tempoFinalPorMundo - tempoInicialPorMundo) / 1000)
+                    + " segundos para ser minerado");
+        } // for dos mundos
+
         Long tempoFinal = System.currentTimeMillis();
-        System.out.println("O tempo total para minerar todos os servidores foi de " + ((tempoFinal - tempoInicial)/1000)
-        + " segundos");
-        
-        
+        System.out.println("O tempo total para minerar todos os servidores foi de " + ((tempoFinal - tempoInicial) / 1000)
+                + " segundos");
+
+        System.out.println("___________________________________________________");
         Collections.sort(listaDeChars);
-        
-        for (int i = 0; i < listaDeChars.size() ; i++) {
-            System.out.println((i+1) + " " + listaDeChars.get(i).getName() + " " + listaDeChars.get(i).getLevel() + " " +
-                    listaDeChars.get(i).getWorld());
-            
+
+        for (int i = 0; i < listaDeChars.size(); i++) {
+            System.out.println((i + 1) + " " + listaDeChars.get(i).getName() + " " + listaDeChars.get(i).getLevel() + " "
+                    + listaDeChars.get(i).getWorld());
+
         }
-        
-        
 
     }
 
